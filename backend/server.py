87c63136cf41fa2604be9dc8384a2506cb2007fa
rutujak-bora <rutuja.k@bora.tech>
2026-02-21 18:10:37 +0530

    Fix MongoDB SSL issue by using certifi CA store

--- backend/server.py
+++ backend/server.py
@@ -28,7 +28,7 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(mongo_url, tlsAllowInvalidCertificates=True)
+client = AsyncIOMotorClient(mongo_url, tlsCAFile=certifi.where())
 db = client[os.environ['DB_NAME']]
 
 app = FastAPI()
